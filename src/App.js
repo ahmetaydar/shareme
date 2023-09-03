87c611d2e54a7438f9eff7f88dd49acf76d04897
ahmetaydar <a.aydar2014@gmail.com>
2023-09-03 16:09:46 +0300

    change mainpage url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
-import { Routes, Route, UseNavigate } from "react-router-dom";
-import Login from "./components/Login";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import { Login } from "./components";
 import Home from "./container/Home";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!user) navigate("/login");
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
